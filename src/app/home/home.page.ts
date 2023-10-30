7a449a887463d5be592d58a5131af02ff10d2759
ntorrey <ntorrey@legionaries.org>
2023-10-30 16:33:00 -0400

    Put ion-datetime in modal.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,21 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import {
+  IonButton,
   IonContent,
-  IonDatetime
+  IonDatetime, IonModal
 } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   template: `
     <ion-content [fullscreen]="true">
-      <ion-datetime presentation="date" [preferWheel]="true"></ion-datetime>
+      <ion-button (click)="open()">Open</ion-button>
+      <ion-modal [isOpen]="isOpen()" (didDismiss)="isOpen.set(false)" class="date-modal" style="--height: 300px;">
+        <ng-template>
+          <ion-datetime
+            #datetime
+            presentation="date"
+            [preferWheel]="true">
+          </ion-datetime>
+        </ng-template>
+      </ion-modal>
     </ion-content>
   `,
   standalone: true,
-  imports: [IonContent, IonDatetime],
+  imports: [IonContent, IonDatetime, IonModal, IonButton],
 })
 export class HomePage {
-
+  isOpen = signal(false)
   constructor() {}
+
+  open() {
+    this.isOpen.set(true)
+  }
 
 }
