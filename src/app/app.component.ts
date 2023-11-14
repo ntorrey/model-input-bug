9d07744015db053b62e51fa03529c434e2ecf747
ntorrey <ntorrey@legionaries.org>
2023-11-14 09:34:14 -0500

    Simply example.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
-
+import { Component, signal } from '@angular/core';
+//import { IonicModule } from '@ionic/angular';
+import { IonApp, IonRouterOutlet, IonButton, IonContent, IonDatetime, IonModal } from '@ionic/angular/standalone';
 @Component({
   selector: 'app-root',
   template: `
     <ion-app>
-      <ion-router-outlet></ion-router-outlet>
+      <ion-content [fullscreen]="true">
+        <ion-button (click)="isOpen.set(true)">Open</ion-button>
+        <ion-modal [isOpen]="isOpen()" (didDismiss)="isOpen.set(false)" class="date-modal" style="--height: 300px;">
+          <ng-template>
+            <ion-content>
+              <ion-datetime
+                #datetime
+                presentation="date"
+                [preferWheel]="true">
+              </ion-datetime>
+            </ion-content>
+          </ng-template>
+        </ion-modal>
+      </ion-content>
     </ion-app>
+    
   `,
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [
+    //IonicModule,
+    IonApp, IonRouterOutlet, IonContent, IonDatetime, IonModal, IonButton
+  ],
 })
 export class AppComponent {
-  constructor() {}
+  isOpen = signal(false)
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter } from '@angular/router';
+import { RouteReuseStrategy } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
-import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { IonicModule } from '@ionic/angular';
 
 if (environment.production) {
   enableProdMode();
@@ -14,7 +14,31 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
-    provideRouter(routes),
+   /* importProvidersFrom([
+      IonicModule.forRoot({
+        mode: 'md',
+        animated: true,
+        platform: {
+          desktop: (win) => {
+            const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+                win.navigator.userAgent
+            )
+            return !isMobile
+          }
+        }
+      }),
+    ]),*/
+    provideIonicAngular({
+      mode: 'md',
+      animated: true,
+      platform: {
+        desktop: (win) => {
+         const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+             win.navigator.userAgent
+         )
+         return !isMobile
+        }
+      }
+    })
   ],
 });
